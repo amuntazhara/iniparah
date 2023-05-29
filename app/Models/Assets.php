9bd757d84da26d1b721f95e6ac922164b4231d18
amuntazhara <118533364+amuntazhara@users.noreply.github.com>
2023-05-29 20:19:50 +0700

    fix minor

--- app/Models/Assets.php
+++ app/Models/Assets.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assets extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
 	protected $table = "assets";
 	public static $tableName = "assets";
